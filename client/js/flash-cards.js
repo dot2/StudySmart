feb3a5d8618e59f9762638399b3704d05eea6e19
Dylan D <dotolo.dylan2@gmail.com>
2016-02-17 21:24:32 -0600

    Added hover effects to decks and started layout for individual card views for questions and answers

--- client/js/flash-cards.js
+++ client/js/flash-cards.js
@@ -1,4 +1,10 @@
-Meteor.subscribe('flashcards');
+Template.UserHome.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        self.subscribe('flashcards');
+    });
+});
+
 
 Template.UserHome.helpers({
     flashcards: ()=> {
